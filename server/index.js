d8653e3d9136c70ffbd329e44e83016525d3f037
Mohamedsalbi <73004418+vulture990@users.noreply.github.com>
2021-06-11 00:00:01 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,22 +1,37 @@
 const express=require('express');
 const mysql=require("mysql");
+const cors = require('cors');
+const dotenv = require('dotenv');
 
 const app=express();
+dotenv.config({ path : './.env'});
+
 app.use(express.json());
+app.use(cors());
 const db=mysql.createConnection({
-    user:"root",
-    host:"localhost",
-    password:"password",
-    database:"pfadatabase"
+   host: 'localhost',
+   user: 'root',
+   password: '',
+   database: 'pfa'
 
 });
+db.connect((error)=>{
+         if(error){
+                 console.log('Error');
+         } else {
+                 console.log('MYSQL is Connected');   
+         }
+});
 app.post("/register",(req,res)=>{
-    const status=req.body.status
-    const email=req.body.username
+    console.log(req.body);
+    const selectValue=req.body.selectValue
+    const username =req.body.username
     const password=req.body.password
-    db.query("INSERT INTO users (Status,Email,Password) VALUES (?,?,?)",[status,email,password]);
 
+    db.query("INSERT INTO user (status,email,password) VALUES (?,?,?)",[selectValue,username,password],(err,result)=>{
+        console.log(err);
+    });
 })
 app.listen(3001,()=>{
-    console.log("running on 3301");
+    console.log("running on 3001");
 });
